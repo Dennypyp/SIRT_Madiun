1fd42d0be12eb25eb8003bca03783f6313baa8e1
Dennypyp <denny.pyp11@gmail.com>
2021-02-18 00:52:07 +0700

    first commit

---
+++ resources/views/frontend/include/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="az-footer ht-40">
+    <div class="container ht-100p pd-t-0-f">
+      <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin templates</a> from Bootstrapdash.com</span>
+    </div><!-- container -->
+  </div><!-- az-footer -->
---
+++ routes/frontend.php
@@ -0,0 +1,4 @@
+<?php 
+Route::resource('test', 'frontend\FrontendController');
+
+?>
