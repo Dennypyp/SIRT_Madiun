0e2f0011b410ab03d2786972f7da2d8e70c6bef5
Dennypyp <denny.pyp11@gmail.com>
2021-02-27 22:28:11 +0700

    tambah migration (denny)

---
+++ database/migrations/2021_02_27_152107_create_saldo_pengeluaran_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaldoPengeluaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saldo_pengeluaran', function (Blueprint $table) {
+            $table->id();
+            $table->increments('id');
+            $table->unsignedInteger('saldo_id')->index()->nullable();
+            $table->unsignedInteger('pengeluaran_id')->index()->nullable();
+            $table->foreign('saldo_id')
+                ->references('id')
+                ->on('saldo');
+            $table->foreign('pengeluaran_id')
+                ->references('id')
+                ->on('pengeluaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saldo_pengeluaran');
+    }
+}
