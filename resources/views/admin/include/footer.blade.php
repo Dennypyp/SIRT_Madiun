424e806a95f9527cecc6513eb4d2e061cdf4d124
Dennypyp <denny.pyp11@gmail.com>
2021-03-09 19:41:26 +0700

    Change Login UI, change footer admin (denny)

--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2020</span>
+            <span>Copyright © RT.03/RW.01 2021</span>
         </div>
     </div>
 </footer>
